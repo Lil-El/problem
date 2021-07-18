0329a7dae2eb4dde7cf516267916ddd78eae9234
Lil-El <yxd99324@qq.com>
2021-07-18 22:40:06 +0800

    逗号运算符

---
+++ 6.js
@@ -0,0 +1,9 @@
+let num = 0;
+let fn = ()=>{
+    if(num = false, 1 === 1){
+        console.log('1 === 1');
+        console.log(num);
+    }
+    return num, 'return value';
+}
+console.log(fn());
---
+++ Readme.md
@@ -0,0 +1,6 @@
+# 前端遇到的一些疑问总结
+
+- 1. 异步的串行、并行执行
+- 2. requestAnimateFrame调试
+- 5. Promise.all实现
+- 6. 逗号运算符
